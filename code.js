ee9f5a51dc7deeb14c5efd64a10869621e4bdf51
yancymin <senseslofter@gmail.com>
2020-07-11 01:36:06 +0800

    feat(v1): done

--- code.js
+++ code.js
@@ -1,29 +1,55 @@
-// This plugin will open a modal to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__, { width: 320, height: 400 });
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
+figma.showUI(__html__, { width: 320, height: 480 });
+
+let selectionArr = figma.currentPage.selection;
+
 figma.ui.onmessage = msg => {
-    // One way of distinguishing between different types of messages sent from
-    // your HTML page is to use an object with a "type" property like this.
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
+    if (msg.inputData) {
+        if (selectionArr[0]) {
+            if (figma.currentPage.selection[0].type != 'TEXT') {
+                figma.notify('📌 Select text layers', { timeout: 1 })
+            }
+        } else {
+            figma.notify('📌 Select text layers', { timeout: 1 })
         }
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
+
+        figma.on('selectionchange', () => {
+            if (figma.currentPage.selection[0]) {
+                if (figma.currentPage.selection[0].type === 'TEXT') {
+                    selectionArr = []
+                    if (figma.currentPage.selection.length > 0) {
+                        figma.currentPage.selection.forEach(item => {
+                            selectionArr.push(item)
+                        })
+                    } else {
+                        selectionArr = figma.currentPage.selection[0]
+                    }
+                }
+                else {
+                    figma.notify('📌 Select text layers', { timeout: 1 })
+                }
+            }
+        })
+
+        async function nodeFont(nodeItem) {
+            await figma.loadFontAsync(nodeItem.fontName)
+            nodeItem.characters = msg.inputData;
+            selectionArr = []
+        }
+
+        selectionArr.forEach(node => {
+
+            if (node.lenth > 0) {
+
+                node.forEach(item => {
+                    nodeFont(item)
+                })
+            } else {
+                nodeFont(node)
+            }
+        });
+
+    } else {
+        figma.notify('🤷 No data', { timeout: 1 })
     }
-    // Make sure to close the plugin when you're done. Otherwise the plugin will
-    // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
+
 };
